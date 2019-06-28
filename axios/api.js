81f0ceaac6a76416ff59007758a106068c0a8d01
bzzzzz01 <905161357@qq.com>
2019-06-28 16:25:01 +0800

    update api

--- axios/api.js
+++ axios/api.js
@@ -1,10 +1,10 @@
 /*
- * @Description: axios 封装的公共方法
+ * @Description: axios
  */
 
 import axios from 'axios'
 import config from './config.js'
-import qs from 'qs' // 序列化请求数据，视服务端的要求
+import qs from 'qs'
 
 import { Toast } from 'vant';
 
@@ -18,19 +18,7 @@ export default function $axios (options) {
     )
     instance.interceptors.request.use(
         config => {
-			// Tip: 1
-			// 请求开始的时候可以结合 vuex 开启全屏的 loading 动画
-
-			// Tip: 2 
-			// 带上 token , 可以结合 vuex 或者重 localStorage
-			// if (store.getters.token) {
-			//     config.headers['X-Token'] = getToken() // 让每个请求携带token--['X-Token']为自定义key 请根据实际情况自行修改
-			// } else {
-			//     // 重定向到登录页面    
-			// }
-
-			// Tip: 3
-			// 根据请求方法，序列化传来的参数，根据后端需求是否序列化
+			// Serialize
 			if (config.method.toLocaleLowerCase() === 'post' 
 				|| config.method.toLocaleLowerCase() === 'put' 
 				|| config.method.toLocaleLowerCase() === 'delete') {
@@ -40,40 +28,38 @@ export default function $axios (options) {
           	return config
         },
         error => {
-			// 请求错误时做些事(接口错误、超时等)
-			// Tip: 4
-			// 关闭loadding
+			// Close loadding
 			console.log('request:', error) 
 		
-			//  1.判断请求超时
+			//  if timeout
 			if (error.code === 'ECONNABORTED' && error.message.indexOf('timeout') !== -1) {
-				console.log('根据你设置的timeout/真的请求超时 判断请求现在超时了，你可以在这里加入超时的处理方案')
-				// return service.request(originalRequest);//例如再重复请求一次
+				console.log('请求超时')
+				// return service.request(originalRequest); // once time
 			}
-			//  2.需要重定向到错误页面
+
+			//  redirect
 			const errorInfo = error.response
 			console.log(errorInfo)
 			if (errorInfo) {
-				// error =errorInfo.data//页面那边catch的时候就能拿到详细的错误信息,看最下边的Promise.reject
-				const errorStatus = errorInfo.status; // 404 403 500 ... 等
+				// error =errorInfo.data
+				const errorStatus = errorInfo.status; // 404 403 500 ... 
 				router.push({
 					path: `/error/${errorStatus}`
 				})
 			}
-			return Promise.reject(error) // 在调用的那边可以拿到(catch)你想返回的错误信息
+			return Promise.reject(error) // catch error
         }
     )
   
     instance.interceptors.response.use(
         response => {
 			let data;
-			// IE9时response.data是undefined，因此需要使用response.request.responseText(Stringify后的字符串)
 			if(response.data == undefined){
 				data = response.request.responseText
 			} else{
 				data = response.data
 			}
-			// 根据返回的code值来做不同的处理（和后端约定）
+			// code from back-end
 
 			data = JSON.parse(data);
 			switch (data.code) {
@@ -86,7 +72,7 @@ export default function $axios (options) {
 					return Promise.reject(message);
 				default:
 			}
-			// 若不是正确的返回code，且已经登录，就抛出错误
+			// wrong code
 			// const err = new Error(data.description)
 
 			// err.data = data
@@ -103,7 +89,7 @@ export default function $axios (options) {
 					break
 			
 					case 401:
-					err.message = '未授权，请登录'
+					err.message = '请先登录'
 					break
 			
 					case 403:
@@ -119,7 +105,7 @@ export default function $axios (options) {
 					break
 			
 					case 500:
-					err.message = '服务器内部错误'
+					err.message = '内部错误'
 					break
 			
 					case 501:
@@ -139,7 +125,7 @@ export default function $axios (options) {
 					break
 			
 					case 505:
-					err.message = 'HTTP版本不受支持'
+					err.message = 'HTTP版本不支持'
 					break
 			
 					default:
@@ -147,11 +133,11 @@ export default function $axios (options) {
 			}
 			console.error(err)
 			// Message.error(`ERROR: ${err}`);
-			return Promise.reject(err) // 返回接口返回的错误信息
+			return Promise.reject(err) 
         }
     )
   
-    //请求处理
+    // response
     instance(options)
         .then((res) => {
 			resolve(res)
